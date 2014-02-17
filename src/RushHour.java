c18ea88285eadd1fb4422246e9892690a125ea28
Srabrioux <samuel.rabrioux@iut-valence.fr>
2014-02-17 11:01:03 +0100

    Mise en place du commentaire Javadoc et de ligne de test du programme.

--- src/RushHour.java
+++ src/RushHour.java
@@ -6,6 +6,6 @@ public class RushHour
 {
 	public void jouer()
 	{
-		
+		System.out.println("La partie commence.");
 	}
 }
--- src/lanceurPartieRushHour.java
+++ src/lanceurPartieRushHour.java
@@ -7,7 +7,10 @@ public class lanceurPartieRushHour
 
 	public static void main(String[] a) 
 	{
+		System.out.println("Je vais créer une nouvelle partie.");
 		RushHour rushHourPartie = new RushHour();
+		System.out.println("L'objet RushHourPartie a été créé.");
 		rushHourPartie.jouer();
+		System.out.println("La partie est finis.");
 	}
 }
